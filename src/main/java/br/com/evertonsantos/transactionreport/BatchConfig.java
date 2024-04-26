eb671620324239ae727f5255e18109e3d4d467b1
evertonlsds <everton0102@gmail.com>
2024-04-26 17:14:50 -0300

    first commit

---
+++ src/main/java/br/com/evertonsantos/transactionreport/BatchConfig.java
@@ -0,0 +1,46 @@
+package br.com.evertonsantos.transactionreport;
+
+
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfig {
+    private PlatformTransactionManager transactionManager;
+    private JobRepository jobRepository;
+
+    public BatchConfig(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
+        this.transactionManager = transactionManager;
+        this.jobRepository = jobRepository;
+    }
+
+    @Bean
+    Job job(Step step){
+        return (Job) new JobBuilder("job", jobRepository)
+        .start(step)
+        .incrementer(new RunIdIncrementer())
+        .build();
+
+
+    }
+    @Bean
+    Step step(ItemReader reader, ItemProcessor processor, ItemWriter writer ){
+        return new StepBuilder("step", jobRepository)
+        .chunk(1000, transactionManager)
+        .reader(reader)
+        .processor(processor)
+        .writer(writer)
+        .build();
+    }
+}
---
+++ src/main/java/br/com/evertonsantos/transactionreport/TransactionReportApplication.java
@@ -0,0 +1,13 @@
+package br.com.evertonsantos.transactionreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionReportApplication.class, args);
+	}
+
+}
