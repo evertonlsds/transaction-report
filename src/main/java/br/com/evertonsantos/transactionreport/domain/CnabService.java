f69e230a6dad82ea4fa8399d376a5b1141b73640
evertonlsds <everton0102@gmail.com>
2024-05-13 12:14:10 -0300

    uploadfile

--- src/main/java/br/com/evertonsantos/transactionreport/domain/CnabService.java
+++ src/main/java/br/com/evertonsantos/transactionreport/domain/CnabService.java
@@ -1,10 +1,24 @@
 package br.com.evertonsantos.transactionreport.domain;
 
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 public class CnabService {
-    public void uploadCnabFile(MultipartFile file){
-        
+    private final Path fileStorogeLocation;
+
+    public CnabService(@Value("${file.upload-dir}") String fileUploadDir) {
+        this.fileStorogeLocation = Paths.get(fileUploadDir);
     }
-    
+
+    public void uploadCnabFile(MultipartFile file) throws Exception {
+        var fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        var targetLocation = fileStorogeLocation.resolve(fileName);
+        file.transferTo(targetLocation);
+    }
+
 }
