d0ea3e61ac1d429c725e8f2fda8279bde46f12b4
evertonlsds <everton0102@gmail.com>
2024-05-15 22:10:59 -0300

    adding negative and positive value

--- src/main/java/br/com/evertonsantos/transactionreport/service/TransacaoService.java
+++ src/main/java/br/com/evertonsantos/transactionreport/service/TransacaoService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import br.com.evertonsantos.transactionreport.entity.TipoTransacao;
 import br.com.evertonsantos.transactionreport.entity.TransacaoReport;
 import br.com.evertonsantos.transactionreport.repository.TransacaoRepository;
 
@@ -25,13 +26,16 @@ public class TransacaoService {
 
         transacoes.forEach(transacao -> {
             String nomeDaLoja = transacao.nomeDaLoja();
-            BigDecimal valor = transacao.valor();
+            var tipoTransacao = TipoTransacao.findByTipo(transacao.tipo());
+            BigDecimal valor = transacao.valor().multiply(
+                tipoTransacao.getSinal()
+            );
 
             reportMap.compute(nomeDaLoja, (key, existingReport) -> {
                 var report = (existingReport != null) ? existingReport
                         : new TransacaoReport(key, BigDecimal.ZERO, new ArrayList<>());
-
-                return report.addTotal(valor).addTransacao(transacao);
+                 
+                return report.addTotal(valor).addTransacao(transacao.withValor(valor));
 
             });
         });
